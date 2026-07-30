ca7fd221cfaf427e69e915747e0ee98051c28447
kushal S <kushals09087@gmail.com>
2026-07-30 14:56:07 +0530

    Imported the component and rendered it

---
+++ Day/Day 13/day13/src/App.jsx
@@ -0,0 +1,32 @@
+import Button from "./components/Functional component"
+import  { LeftArrow, RightArrow} from "./components/Functional Component2"
+const name = "Kushal";
+const isLoggedIn = true;
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* JSX Rule 1: Return one parent element  */}
+     <h1>Hello</h1>
+    
+    {/* Rule2: Every tag must be closed */}
+    <p>Error</p> 
+    <input type="text"/>
+    {/* Rule3: use CamelCase for attributes */}
+    <button onClick onSubmit>Click</button>
+
+    {/* Rule 4: Embedding JS with curly braces */}  
+    <h3> Hello, {name}</h3>
+
+    {/* Rule 5: use expressions, not statements */}
+    <p>{isLoggedIn ? "Welcome Back" : "Please Login"} </p>
+
+    <button />
+    <LeftArrow />
+    <RightArrow />
+    </> 
+  )
+}
+
+export default App
